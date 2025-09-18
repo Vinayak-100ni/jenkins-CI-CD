eca26b145f929322a693dd24475c847528055d7e
root <root@ip-172-31-40-230.eu-north-1.compute.internal>
2025-09-18 06:37:30 +0000

    adding terraform

---
+++ jenkins-cicd/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region"{
+ description = "Aws region"
+ default = "eu-north-1"
+}
+
+variable "ami_id"{
+ default = "ami-0a716d3f3b16d290c"
+}
+
+variable "instance_type"{
+ default = "t3.medium"
+}
+
+
